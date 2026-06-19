7b034831f75d5b35302055766b132bff4f578f6e
erikandgabriel <eeu31471@gmail.com>
2026-06-19 09:44:35 -0300

    Add main and OSINT menus to ui/menus.py
    Define multiple menus for user interaction.

---
+++ ui/menus.py
@@ -0,0 +1,26 @@
+menu_principal = f"""
+[1] PHISHING.   [2] OSINT
+[3] SCANNER.    [4] INSTALL
+[5] MALWARES.   [6] SQL
+[7] XSS.        [8] LORKUP
+[9] EXIT"""
+
+menu_osint = f"""
+[1] ZPHISHING.  [2] FOTO
+[3] INFO.       [4] NETESCOLA
+[5] AUDIO.      [6] EXIT
+"""
+
+menu_osint = f"""
+[01] IP INFO  [02] EMAIL
+[03] WHOIS    [04] GOOGLE DORK
+[05] USERNAME [06] CNPJ
+[07] CEP.     [08] DDD/DDI
+[09] GITHUB.  [10] INSTAGRAM
+[11] DIRB.    [00] EXIT
+"""
+
+menu_scanner = f"""
+[01] NMAP
+[00] EXIT
+"""
