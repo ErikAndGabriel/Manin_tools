61d6d86cb9f92c5e730815a4fb79d9419d80a8d0
erikandgabriel <eeu31471@gmail.com>
2026-06-23 10:47:50 -0300

    Update menus.py

--- ui/menus.py
+++ ui/menus.py
@@ -5,7 +5,7 @@ menu_principal = f"""
 [7] XSS.        [8] LORKUP
 [9] EXIT"""
 
-menu_osint = f"""
+menu_phishing = f"""
 [1] ZPHISHING.  [2] FOTO
 [3] INFO.       [4] NETESCOLA
 [5] AUDIO.      [6] EXIT
@@ -17,10 +17,20 @@ menu_osint = f"""
 [05] USERNAME [06] CNPJ
 [07] CEP.     [08] DDD/DDI
 [09] GITHUB.  [10] INSTAGRAM
-[11] DIRB.    [00] EXIT
+[11] DIRB.    [12] DNS
+[13] REDDIT.  [00] EXIT
 """
 
 menu_scanner = f"""
 [01] NMAP
+[02] SCAN
 [00] EXIT
 """
+
+menu_sqlmap = f"""
+"""
+menu_lorkup = f"""
+[01] INSTAGRAM [02] XVIDEOS [03] INFO_PESSOA
+[04] FACEBOOK  [05] GMAIL.  [06] REDDIT
+[07] PORNHUB.  [08] GOV.    [09] NETESCOLA
+[10] 
