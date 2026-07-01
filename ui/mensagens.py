efc086058f8802afe0ad06d10d6f08a7bb16bc57
erikandgabriel <eeu31471@gmail.com>
2026-07-01 15:54:18 -0300

    Create mensagens.py

---
+++ ui/mensagens.py
@@ -0,0 +1,10 @@
+from color.py import vermelho, verde, amarelo 
+
+def erro(data):
+  print(vermelho, data)
+
+def sucesso(data):
+  print(verde, data)
+
+def mensagem(data):
+  print(amarelo, data)
