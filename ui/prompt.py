698de3751b4fbf98b97406d35f247cffd89e9770
erikandgabriel <eeu31471@gmail.com>
2026-07-03 21:20:35 -0300

    Update prompt.py

--- ui/prompt.py
+++ ui/prompt.py
@@ -2,3 +2,4 @@ from color import azul, amarelo, reset, verde
 
 prompt_menu = f"{azul}Olhar {verde}~/ {azul}> "
 prompt_cpf = f"{azul}Olhar {verde}modulos/osint/cpf {azul}> "
+prompt_osint = f"{azul}Olhar {verde}modulos/osint {azul}> "
