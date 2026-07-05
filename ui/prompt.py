a86e8787db12200dcb3cf2460da53c5a08ad88c2
erikandgabriel <eeu31471@gmail.com>
2026-07-05 05:12:36 -0300

    Update import path for color module in prompt.py

--- ui/prompt.py
+++ ui/prompt.py
@@ -1,4 +1,4 @@
-from color import azul, amarelo, reset, verde
+from ui.color import azul, amarelo, reset, verde
 
 prompt_menu = f"{azul}Olhar {verde}~/ {azul}> "
 prompt_cpf = f"{azul}Olhar {verde}modulos/osint/cpf {azul}> "
