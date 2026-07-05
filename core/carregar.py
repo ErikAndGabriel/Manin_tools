0432c8b1830cfc850389ee31dbb9a27c72a89ce4
erikandgabriel <eeu31471@gmail.com>
2026-07-05 18:08:41 -0300

    Update carregar.py

--- core/carregar.py
+++ core/carregar.py
@@ -1,6 +1,11 @@
 def carregar_wordlist(wordlist):
-  with open(f"../data/wordlist/{wordlist}", "r") as arq:
-    lista = []
+  try:
+    with open(f"data/wordlist/{wordlist}", "r") as arq:
+      lista = []
     for linha in arq:
       lista.append(linha.strip())
     return lista
+  except FileNotFoundError:
+    return "erro, a worlist não foi encontrada"
+  except Exception as e:
+    return f"erro detectado: {e}"
