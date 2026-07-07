d7279a553b701726988944386674bb3518db0492
erikandgabriel <eeu31471@gmail.com>
2026-07-07 03:53:44 -0300

    Create ip.py

---
+++ modulos/osint/ip.py
@@ -0,0 +1,49 @@
+from config.apis import APIS
+from core.config import Timeout, Headers
+import json 
+import requests 
+
+class Ip:
+  def __init__(self, ip, arquivo):
+    self.ip = ip
+    self.arq = arquivo
+    self.timeout = Timeout("api")
+    self.headers = Headers("aleatorio")
+  def IpBusca(self):
+    url = APIS["api_ip"]["url"].format(self.ip)
+    resposta = requests.get(
+      url, 
+      timeout=self.timeout,
+      headers=self.headers
+    )
+    if resposta.status_code == 200:
+      data = resposta.json()
+      print(f"""
+      api em uso.......: {APIS["api_ip"]}
+      ip...............: {data["ip"]}
+      sucesso..........: {data["sucess"]}
+      tipo.............: {data["type"]}
+      continente.......: {data["continent"]}
+      codigo continente: {data["continent_code"]}
+      região...........: {data["region"]}
+      codigo região....: {data["region_code"]}
+      cidade...........: {data["city"]}
+      coordenadas......: {data["latitude"]}, {data["latitude"]}
+      is eu............: {data["is_eu"]}
+      postal...........: {data["postal"]}
+      calling_code.....: {data["calling_code"]}
+      capital..........: {data["capital"]}
+      borders..........: {data["borders"]}
+      img..............: {data["flag"]["img"]}
+      emoje............: {data["flag"]["emoje"]}
+      emoji_unicode....: {data["flag"]["emoji_unicode"]}
+      asn..............: {data["connection"]["asn"]}
+      org..............: {data["connection"]["org"]}
+      isp..............: {data["connection"]["isp"]}
+      domínio..........: {data["connection"]["domain"]}
+      id...............: {data["timezone"]["id"]}
+      addr.............: {data["timezone"]["addr"]}
+      is dst...........: {data["timezone"]["is_dst"]}
+      offset...........: {data["timezone"]["offset"]}
+      utc..............: {data["timezone"]["utc"]}
+      """
