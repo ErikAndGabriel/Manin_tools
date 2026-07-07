b12a6d96ef0be0d55cb3a92582d3fe19436874d3
erikandgabriel <eeu31471@gmail.com>
2026-07-07 04:04:51 -0300

    Fix import statement for SisregConsulta

--- MainTools.py
+++ MainTools.py
@@ -1,4 +1,4 @@
 from ui.menus import menu_principal, menu_osint 
 from ui.prompt import prompt_menu, prompt_osint, prompt_cpf
-from modulos.osint.cpf import SisregConsulta  # Fixed import
+from modulos.osint.cpf import SisregConsulta  
 
