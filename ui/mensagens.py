bb9a46ac2f7f6d122e6289e2a19523184e8c0131
erikandgabriel <eeu31471@gmail.com>
2026-07-07 06:12:10 -0300

    Add global function for user input pause
    Added a global function to pause execution after printing messages.

--- ui/mensagens.py
+++ ui/mensagens.py
@@ -1,13 +1,19 @@
 from ui.color import vermelho, verde, amarelo, roxo 
 
+def global():
+  input("precione [ENTER]")
+  
 def erro(data):
   print(vermelho, data)
-
+  global()
+  
 def sucesso(data):
   print(verde, data)
-
+  global()
+  
 def mensagem(data):
   print(amarelo, data)
-
+  
 def personalizar(data):
   print(roxo, data)
+  global()
