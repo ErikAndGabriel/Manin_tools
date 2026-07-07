839c3cb9e72572acdb31a3a826f750a1733c9779
erikandgabriel <eeu31471@gmail.com>
2026-07-07 07:06:38 -0300

    Create apis_bin.py with BIN API configurations
    Added a dictionary of API configurations for BIN services.

---
+++ config/APIS/apis_bin.py
@@ -0,0 +1,18 @@
+APIS_BIN = {
+    "binlist": {
+        "nome": "BINList",
+        "url": "https://lookup.binlist.net/{}"
+    },
+    "bincheck": {
+        "nome": "BINCheck",
+        "url": "https://bincheck.io/api/v1/{}"
+    },
+    "bins": {
+        "nome": "BINS",
+        "url": "https://api.bins.dev/{}"
+    },
+    "freebinchecker": {
+        "nome": "Free BIN Checker",
+        "url": "https://api.freebinchecker.com/bin/{}"
+    }
+}
