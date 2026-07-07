e1b287bab39862303000cff66a3779a6a8bb6581
erikandgabriel <eeu31471@gmail.com>
2026-07-07 07:01:53 -0300

    Add API configurations for various CEP services

---
+++ config/APIS/api_cep.py
@@ -0,0 +1,18 @@
+APIS_CEP = {
+    "viacep": {
+        "nome": "ViaCEP",
+        "url": "https://viacep.com.br/ws/{}/json/"
+    },
+    "brasilapi": {
+        "nome": "BrasilAPI",
+        "url": "https://brasilapi.com.br/api/cep/v1/{}"
+    },
+    "apicep": {
+        "nome": "ApiCEP",
+        "url": "https://cdn.apicep.com/file/apicep/{}.json"
+    },
+    "cepaberto": {
+        "nome": "CEP Aberto",
+        "url": "https://www.cepaberto.com/api/v3/cep?cep={}"
+    }
+}
