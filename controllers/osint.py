8db2e2fd6eb8829120d1f139f54155b693e68b85
erikandgabriel <eeu31471@gmail.com>
2026-07-07 10:43:58 -0300

    Update osint.py

--- controllers/osint.py
+++ controllers/osint.py
@@ -1,1 +1,13 @@
-.
+from ui.menus import menu_osint
+from ui.prompt import prompt_osint
+from ui.mensagens import erro
+from modulos.osint.ip import Ip
+
+def painel_osint():
+  while True:
+    try:
+      print(menu_osint)
+      ot = int(input(prompt_osint))
+    except ValueError:
+      erro("erro, somente numeros")
+      
