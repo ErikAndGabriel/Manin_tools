e91033f0aadf687e1636b3f589fa20e3b8a90cd4
erikandgabriel <eeu31471@gmail.com>
2026-07-07 07:05:49 -0300

    Add api_placa.py with API configurations

---
+++ config/APIS/api_placa.py
@@ -0,0 +1,18 @@
+APIS_PLACA = {
+    "api_placas": {
+        "nome": "API Placas",
+        "url": "https://wdapi2.com.br/consulta/{}/TOKEN"
+    },
+    "placas_app": {
+        "nome": "Placas App",
+        "url": "https://api.placas.app.br/consulta/{}"
+    },
+    "fipeapi": {
+        "nome": "FIPE API",
+        "url": "https://placas.fipeapi.com.br/placas/{}?key=API_KEY"
+    },
+    "consultar_placa": {
+        "nome": "Consultar Placa",
+        "url": "https://api.consultarplaca.com.br/v2/consultarPlaca?placa={}"
+    }
+}
