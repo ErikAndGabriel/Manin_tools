2af8b0e75572182ec572c3ed35815fa2f4593a8a
erikandgabriel <eeu31471@gmail.com>
2026-07-07 07:02:34 -0300

    Add CNPJ API configurations
    Added configuration for CNPJ API endpoints.

---
+++ config/APIS/apis_cnpj.py
@@ -0,0 +1,14 @@
+APIS_CNPJ = {
+    "brasilapi": {
+        "nome": "BrasilAPI",
+        "url": "https://brasilapi.com.br/api/cnpj/v1/{}"
+    },
+    "receitaws": {
+        "nome": "ReceitaWS",
+        "url": "https://www.receitaws.com.br/v1/cnpj/{}"
+    },
+    "cnpjws": {
+        "nome": "CNPJ.ws",
+        "url": "https://publica.cnpj.ws/cnpj/{}"
+    }
+}
