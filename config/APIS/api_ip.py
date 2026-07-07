8d7ecf85add6b415789d891c5a83423781740ed8
erikandgabriel <eeu31471@gmail.com>
2026-07-07 07:01:08 -0300

    Create api_ip.py

---
+++ config/APIS/api_ip.py
@@ -0,0 +1,30 @@
+APIS_IP = {
+    "ipwhois": {
+        "nome": "ipwhois",
+        "url": "https://ipwho.is/{}"
+    },
+    "ipapi": {
+        "nome": "ipapi",
+        "url": "https://ipapi.co/{}/json/"
+    },
+    "ipinfo": {
+        "nome": "ipinfo",
+        "url": "https://ipinfo.io/{}/json"
+    },
+    "freeipapi": {
+        "nome": "freeipapi",
+        "url": "https://freeipapi.com/api/json/{}"
+    },
+    "ip_api": {
+        "nome": "ip-api",
+        "url": "http://ip-api.com/json/{}"
+    },
+    "ipdata": {
+        "nome": "ipdata",
+        "url": "https://api.ipdata.co/{}?api-key={}"
+    },
+    "abstractapi": {
+        "nome": "abstractapi",
+        "url": "https://ipgeolocation.abstractapi.com/v1/?api_key={}&ip_address={}"
+    }
+}
