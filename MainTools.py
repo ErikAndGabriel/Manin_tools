fd45c11b0b06c4db3ed8b59aabc100e182c5dc58
erikandgabriel <eeu31471@gmail.com>
2026-07-07 06:09:06 -0300

    Improve error handling in MainTools.py
    Added error handling for invalid choices and non-numeric input.

--- MainTools.py
+++ MainTools.py
@@ -1,6 +1,6 @@
 from ui.menus import menu_principal
 from ui.prompt import prompt_menu
-
+from ui.mensagens import erro
 opcoes = {
   "1": "phishing",
   "2": "osint"
@@ -13,4 +13,10 @@ def inicial():
       ot = int(input(prompt_menu)
       for escolha in opcoes.values():
         if ot == opcoes[str(ot)]:
-               opcoes[str(ot)]()
+          opcoes[str(ot)]()
+      else:
+        erro("escolha invalida")
+        continue 
+    except ValueError:
+      erro("erro, somente numeros")
+      continue
