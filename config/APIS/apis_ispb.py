0bc73bd87438edd18a940ab0705db3f29ac7a940
erikandgabriel <eeu31471@gmail.com>
2026-07-07 07:13:01 -0300

    Add APIS_ISPB configuration for bank APIs

---
+++ config/APIS/apis_ispb.py
@@ -0,0 +1,14 @@
+APIS_ISPB = {
+    "brasilapi": {
+        "nome": "BrasilAPI",
+        "url": "https://brasilapi.com.br/api/banks/v1"
+    },
+    "bacen": {
+        "nome": "Banco Central",
+        "url": "https://www.bcb.gov.br/api/servico/sitebcb/participantes"
+    },
+    "openfinance": {
+        "nome": "Open Finance Brasil",
+        "url": "https://data.directory.openbankingbrasil.org.br/participants"
+    }
+}
