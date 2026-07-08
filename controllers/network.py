081804e5206081a514faa0660ded130e36293c0e
erikandgabriel <eeu31471@gmail.com>
2026-07-08 13:13:43 -0300

    Add clear function to network panel

--- controllers/network.py
+++ controllers/network.py
@@ -1,12 +1,15 @@
 from ui.menus import menu_network
 from ui.mensagens import erro
 from ui.prompt import prompt_network
-
+from core.clear import clear
 def painel_network():
   while True:
     try:
       print(menu_network)
       ot = int(input(prompt_network)
+      if ot == 0:
+        clear()
+        break
     except ValueError:
       erro("somente numeros")
     except Exception as e:
