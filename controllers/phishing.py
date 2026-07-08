54cdb7bcfd290d6862c6dda666e9130781165f46
erikandgabriel <eeu31471@gmail.com>
2026-07-08 13:12:45 -0300

    Update phishing.py

--- controllers/phishing.py
+++ controllers/phishing.py
@@ -2,12 +2,16 @@ from ui.menus import menu_phishing
 from ui.prompt import prompt_phishing
 from ui.mensagens import erro
 from core.carregar import carregar_json 
-
+from core.clear import clear
 def painel_phishing():
   while True:
     try:
       print(menu_phishing)
       ot = int(input(prompt_phishing))
+      if ot == 0:
+        clear()
+        break
+        
     except ValueError:
       erro("somente numeros")
     except Exception as e:
