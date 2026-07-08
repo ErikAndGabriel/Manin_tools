062c6355de0aa9417cde155a24a4d2f2b8b21814
erikandgabriel <eeu31471@gmail.com>
2026-07-08 12:51:29 -0300

    Update prompt.py

--- ui/prompt.py
+++ ui/prompt.py
@@ -2,4 +2,5 @@ from ui.color import azul, amarelo, reset, verde
 
 prompt_menu = f"{azul}Olhar {verde}~/ {azul}> "
 prompt_phishing = f"{azul}Olhar {verde}modulos/phishing {azul}> "
+prompt_network = f"{azul}Olhar {verde}modulos/network {azul}>"
 prompt_osint = f"{azul}Olhar {verde}modulos/osint {azul}> "
