ee4d67abfb24b5ac91407c26ab100451123a3488
erikandgabriel <eeu31471@gmail.com>
2026-07-08 13:08:19 -0300

    Expand OSINT menu with new options
    Added additional OSINT options to the menu.

--- ui/menus.py
+++ ui/menus.py
@@ -34,6 +34,44 @@ HOME
 ├── [09] DOX
 └── [10] EXIT
 """
+menu_ip = f"""{reset}
+HOME
+│
+├─
+├── [02] OSINT
+│   ├── [01] IP INFO
+│   │   ├── {azul}[01]{reset} IPWHO.IS
+│   │   ├── {azul}[02]{reset} IPAPI.CO
+│   │   ├── {azul}[03]{reset} IPINFO.IO
+│   │   ├── {azul}[04]{reset} FREEIPAPI
+│   │   ├── {azul}[05]{reset} IP-API
+│   │   ├── {azul}[06]{reset} IPDATA
+│   │   ├── {azul}[07]{reset} ABSTRACTAPI
+│   │   └── {azul}[00]{reset} VOLTAR
+│   ├── [02] EMAIL (Holehe)
+│   ├── [03] CPF
+│   ├── [04] WHOIS
+│   ├── [05] GOOGLE DORK
+│   ├── [06] NOME
+│   ├── [07] USERNAME
+│   ├── [08] CNPJ
+│   ├── [09] PHONE (API)
+│   ├── [10] CEP
+│   ├── [11] DDD / DDI
+│   ├── [12] NOME PAI
+│   ├── [13] GITHUB
+│   ├── [14] INSTAGRAM
+│   ├── [15] NOME MÃE
+│   ├── [16] DNS
+│   ├── [17] DNS
+│   ├── [18] CNS
+│   ├── [19] REDDIT
+│   ├── [20] CEP (API)
+│   ├── [21] RG
+│   ├── [22] PHONE
+│   ├── [23] EMAIL (API)
+│   └── {azul}[00]{reset} VOLTAR
+"""
 menu_osint = f"""{reset}
 HOME
 │
