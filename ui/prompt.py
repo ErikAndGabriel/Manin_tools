b9cb5eec2362695f5b654277443d44c09783e01c
erikandgabriel <eeu31471@gmail.com>
2026-07-08 13:00:15 -0300

    Update prompt.py

--- ui/prompt.py
+++ ui/prompt.py
@@ -1,6 +1,9 @@
-from ui.color import azul, amarelo, reset, verde
+from ui.color import azul, amarelo, reset, verde, red
 
 prompt_menu = f"{azul}Olhar {verde}~/ {azul}> "
 prompt_phishing = f"{azul}Olhar {verde}modulos/phishing {azul}> "
 prompt_network = f"{azul}Olhar {verde}modulos/network {azul}>"
 prompt_osint = f"{azul}Olhar {verde}modulos/osint {azul}> "
+prompt_web = f"{azul}Olhar {verde}modulos/web {azul}> "
+prompt_lorkup = f"{azul}Olhar {verde}modulos/lorkup {azul}> "
+prompt_malware = f"{red}Olhar {azul}modulos/osint {red}>{verde} "
