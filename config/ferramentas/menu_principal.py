ea3849f67c69d14882f54633c4122f3daae60b43
erikandgabriel <eeu31471@gmail.com>
2026-07-09 02:59:44 -0300

    Add new options for painel_dox and painel_c2

--- config/ferramentas/menu_principal.py
+++ config/ferramentas/menu_principal.py
@@ -4,6 +4,9 @@ from controllers.web import painel_web
 from controllers.network import painel_network
 from controllers.malware import painel_malware
 from controllers.loorkup import painel_lorkup
+from controllers.dox import painel_dox
+from controllers.c2 import painel_c2
+from controllers.install import painel_install
 opcoes = {
   
   "1": painel_phishing,
@@ -11,7 +14,7 @@ opcoes = {
   "3": painel_network,
   "4": painel_install,
   "5": painel_malwares,
-  "6": painel_cs,
+  "6": painel_c2,
   "7": painel_web,
   "8": painel_lorkup,
   "9": painel_dox
