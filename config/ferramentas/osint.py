90d9469f702ad826c4442a39d70025300df047d7
erikandgabriel <eeu31471@gmail.com>
2026-07-09 23:06:54 -0300

    Update osint.py

--- config/ferramentas/osint.py
+++ config/ferramentas/osint.py
@@ -1,6 +1,17 @@
 from controllers.osint.osint_ip import painel_ip
-from config.APIS.apis_ip import APIS_IP
-
+from config.APIS.api_ip import APIS_IP
 ferramentas = {
   "1": painel_ip
+}
+
+ferramentas_ip = {
+  
+  "1": "ipwho.is",
+  "2": "ipapi.co",
+  "3": "ipinfo.io",
+  "4": "freeipapi",
+  "5": "ip-api",
+  "6": "ipdata",
+  "7": "abstractapi"
+  
 }
