a4e0328166377abfb340e4b6b54f4e4dab85671a
erikandgabriel <eeu31471@gmail.com>
2026-07-09 23:38:03 -0300

    Remove ferramentas_ip dictionary from osint.py
    Removed ferramentas_ip dictionary and cleaned up code.

--- config/ferramentas/osint.py
+++ config/ferramentas/osint.py
@@ -2,15 +2,3 @@ from controllers.outros_osint.osint_ip import painel_ip
 ferramentas = {
   "1": painel_ip
 }
-
-ferramentas_ip = {
-  
-  "1": "ipwho.is",
-  "2": "ipapi.co",
-  "3": "ipinfo.io",
-  "4": "freeipapi",
-  "5": "ip-api",
-  "6": "ipdata",
-  "7": "abstractapi"
-  
-}
