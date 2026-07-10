b0dc94a0cc1d4fd7941da0103af2249ab7d2deee
erikandgabriel <eeu31471@gmail.com>
2026-07-10 01:11:36 -0300

    Update FORMATADORES to use new format functions

--- controllers/outros_osint/formatters/formato.py
+++ controllers/outros_osint/formatters/formato.py
@@ -1,6 +1,8 @@
+from format_api import format_ipwhois, format_ipapi, format_freeipapi,format_ip_api, format_ipinfo
 FORMATADORES = {
-    "ipwhois": whois,
-    "ipapi": ipapi,
-    "ipinfo": ipinfo,
-    "freeipapi": freeipapi,
+    "ipwhois": format_ipwhois,
+    "ipapi": format_ipapi,
+    "ipinfo": format_ipinfo,
+    "freeipapi": format_freeipapi,
+    "ip_api": format_ip_api
 }
