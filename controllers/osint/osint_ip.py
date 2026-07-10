d32a6f18fbb304b53b0a8bcbcfffc670b9859285
erikandgabriel <eeu31471@gmail.com>
2026-07-09 23:11:52 -0300

    Enhance painel_ip to process OSINT IP options
    Added functionality to handle user input for OSINT IP tools.

--- controllers/osint/osint_ip.py
+++ controllers/osint/osint_ip.py
@@ -1,9 +1,9 @@
 from ui.menu import menu_ip
 from ui.prompt import prompt_osint
-from ui.mensagens import erro
+from ui.mensagens import erro, outro
 from modulos.osint.ip import Ip
 from core.clear import clear
-
+from config.ferramentas.osint import ferramentas_ip
 
 def painel_ip():
   while True:
@@ -13,6 +13,15 @@ def painel_ip():
       if ot == 0:
         clear()
         break
+        
+      if str(ot) in ferramentas_ip:
+        api = ferramentas_ip[str(ot)]
+        prompt = input("ip > ")
+        user = Ip(prompt, api)
+        user.IpBusca()
+        outro()
+        continue
+        
       else:
         erro("escolha invalida")
         continue
