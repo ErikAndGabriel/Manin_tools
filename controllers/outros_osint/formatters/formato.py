bc87447191949628a63f9a1bb3fb20ed4e890dd1
erikandgabriel <eeu31471@gmail.com>
2026-07-10 22:47:42 -0300

    Delete controllers/outros_osint/formatters directory

--- controllers/outros_osint/formatters/formato.py
+++
@@ -1,8 +0,0 @@
-from .format_api import format_ipwhois, format_ipapi, format_freeipapi,format_ip_api, format_ipinfo
-FORMATADORES = {
-    "ipwhois": format_ipwhois,
-    "ipapi": format_ipapi,
-    "ipinfo": format_ipinfo,
-    "freeipapi": format_freeipapi,
-    "ip_api": format_ip_api
-}
