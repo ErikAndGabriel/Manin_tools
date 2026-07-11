532db803542a3fdf91b21abbd91c218b572caf01
erikandgabriel <eeu31471@gmail.com>
2026-07-11 01:48:14 -0300

    Update menus.py

--- ui/menus.py
+++ ui/menus.py
@@ -34,6 +34,41 @@ HOME
 ├── [09] DOX
 └── [00] EXIT
 """
+menu_cep = f"""{reset}
+HOME
+│
+├─
+├── [02] OSINT
+│   ├── [01] IP INFO
+│   ├── [02] EMAIL (Holehe)
+│   ├── [03] CPF
+│   ├── [04] WHOIS
+│   ├── [05] GOOGLE DORK
+│   ├── [06] NOME
+│   ├── [07] USERNAME
+│   ├── [08] CNPJ
+│   ├── [09] PHONE (API)
+│   ├── [10] CEP
+│   │   ├── {azul}[01]{reset} VIACEP
+│   │   ├── {azul}[02]{reset} BRASILAPI
+│   │   ├── {azul}[03]{reset} APICEP
+│   │   ├── {azul}[04]{reset} CEP ABERTO
+│   │   └── {azul}[00]{reset} VOLTAR
+│   ├── [11] DDD / DDI
+│   ├── [12] NOME PAI
+│   ├── [13] GITHUB
+│   ├── [14] INSTAGRAM
+│   ├── [15] NOME MÃE
+│   ├── [16] DNS
+│   ├── [17] DNS
+│   ├── [18] CNS
+│   ├── [19] REDDIT
+│   ├── [20] CEP (API)
+│   ├── [21] RG
+│   ├── [22] PHONE
+│   ├── [23] EMAIL (API)
+│   └── [00] VOLTAR
+"""
 menu_ip = f"""{reset}
 HOME
 │
@@ -70,7 +105,7 @@ HOME
 │   ├── [21] RG
 │   ├── [22] PHONE
 │   ├── [23] EMAIL (API)
-│   └── {azul}[00]{reset} VOLTAR
+│   └── [00] VOLTAR
 """
 menu_osint = f"""{reset}
 HOME
