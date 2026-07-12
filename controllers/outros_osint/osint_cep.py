887e6b30956c5bb5f229a5e7d3284bd30a8e0b62
erikandgabriel <eeu31471@gmail.com>
2026-07-12 16:51:52 -0300

    Refactor OSINT panel to use CEP instead of IP

--- controllers/outros_osint/osint_cep.py
+++ controllers/outros_osint/osint_cep.py
@@ -1,29 +1,29 @@
-from ui.menus import menu_ip
+from ui.menus import menu_cep
 from ui.prompt import prompt_osint
 from ui.mensagens import erro, outro
 from ui.color import azul
 from modulos.osint.cep import Cep
 from core.clear import clear
-from config.ferramentas.ip import ferramentas_ip
+from config.ferramentas.ip import ferramentas_cep
 from config.APIS.api_cep import APIS_CEP
 def painel_ip():
   while True:
     try:
-      print(menu_ip)
+      print(menu_cep)
       ot = int(input(prompt_osint))
       if ot == 0:
         clear()
         break
         
       if str(ot) in ferramentas_ip:
-        api = ferramentas_ip[str(ot)]
+        api = ferramentas_cep[str(ot)]
         try:
           cep = int(input("cep > "))
         except ValueError:
           erro("somente numeros no cep")
           clear()
           continue 
-        user = Ip(cep, api)
+        user = Cep(cep, api)
         user.CepBusca()
         outro()
         continue
