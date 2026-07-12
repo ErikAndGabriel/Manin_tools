1ff910e35531e586c90e844f3e395500b6378e40
erikandgabriel <eeu31471@gmail.com>
2026-07-12 17:23:44 -0300

    Add new formatters for CEP APIs

--- modulos/osint/formatters/formato.py
+++ modulos/osint/formatters/formato.py
@@ -1,8 +1,15 @@
-from modulos.osint.formatters.formate_api import format_ipwhois, format_ipapi, format_freeipapi,format_ip_api, format_ipinfo
+from modulos.osint.formatters.formate_api import format_ipwhois, format_ipapi, format_freeipapi,format_ip_api, format_ipinfo, format_cepaberto, format_apicep, format_brasilapi, format_viacep
 FORMATADORES = {
     "ipwhois": format_ipwhois,
     "ipapi": format_ipapi,
     "ipinfo": format_ipinfo,
     "freeipapi": format_freeipapi,
     "ip_api": format_ip_api
+}
+
+FORMATADORES_CEP = {
+    "viacep": format_viacep,
+    "brasilapi": format_brasilapi,
+    "apicep": format_apicep,
+    "api_cep": format_api_cep
 }
