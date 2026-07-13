9c4965ee57f1501db45f577504828bf820d57c2a
erikandgabriel <eeu31471@gmail.com>
2026-07-13 02:50:05 -0300

    Fix formatting in ferramentas dictionary

--- config/ferramentas/osint.py
+++ config/ferramentas/osint.py
@@ -2,6 +2,6 @@ from controllers.outros_osint.osint_ip import painel_ip
 from controllers.outros_osint.osint_cep import painel_cep 
 
 ferramentas = {
-  "1": painel_ip
+  "1": painel_ip,
   "2": painel_cep
 }
