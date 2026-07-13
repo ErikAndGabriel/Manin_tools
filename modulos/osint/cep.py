becede34ed418c6da9e1e521653274019b0eff88
erikandgabriel <eeu31471@gmail.com>
2026-07-13 03:11:07 -0300

    Fix URL formatting in CepBusca method

--- modulos/osint/cep.py
+++ modulos/osint/cep.py
@@ -10,7 +10,7 @@ class Cep:
     self.timeout = timeout("api")
     
   def CepBusca(self):
-    url = APIS_CEP[self.api]["url"].format(self.ip)
+    url = APIS_CEP[self.api]["url"].format(self.cep)
     try:
       resposta = requests.get(
         url,
