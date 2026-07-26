242bb29b81e47e1d496754c05e4419b6166b48ef
erikandgabriel <eeu31471@gmail.com>
2026-07-26 13:17:43 -0300

    Add Email class import and usage in painel_email
    Import Email class and instantiate it with user input.

--- controllers/outros_osint/osint_e-mail.py
+++ controllers/outros_osint/osint_e-mail.py
@@ -2,11 +2,12 @@ import os
 from ui.mensagens import erro, outro 
 from ui.menus import menu_osint 
 from ui.prompt import prompt_osint
-
+from modulos.osint.email import Email 
 def painel_email():
   while True:
     try:
       print(menu_osint)
       e-mail = input("e-mail > ")
+      user = Email(e-mail)
     except Exception as e:
       erro(f"erro: {e}")
