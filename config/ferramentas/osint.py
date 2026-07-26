6e50a5e70f18f3fa5bf00400cb4b3f69a6bf065f
erikandgabriel <eeu31471@gmail.com>
2026-07-26 13:06:35 -0300

    Add painel_cpf to ferramentas dictionary

--- config/ferramentas/osint.py
+++ config/ferramentas/osint.py
@@ -1,6 +1,6 @@
 from controllers.outros_osint.osint_ip import painel_ip
 from controllers.outros_osint.osint_cep import painel_cep 
-
+from controllers.outros_osint.osint_cpf import painel_cpf
 ferramentas = {
   "1": painel_ip,
   "3": painel_cpf,
