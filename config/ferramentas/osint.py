2f05323c2cb3fbff015dd786b7e36cd0ccb4377e
erikandgabriel <eeu31471@gmail.com>
2026-07-26 13:20:03 -0300

    Update osint.py

--- config/ferramentas/osint.py
+++ config/ferramentas/osint.py
@@ -1,8 +1,11 @@
 from controllers.outros_osint.osint_ip import painel_ip
 from controllers.outros_osint.osint_cep import painel_cep 
 from controllers.outros_osint.osint_cpf import painel_cpf
+from controllers.outros_osint.osint_e-mail import painel_email
+
 ferramentas = {
   "1": painel_ip,
+  "2": painel_email,
   "3": painel_cpf,
   "10": painel_cep
 }
