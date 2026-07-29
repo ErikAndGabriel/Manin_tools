92d996a918993875526b89c9baac102c3fec78b8
erikandgabriel <eeu31471@gmail.com>
2026-07-29 01:44:43 -0300

    Add osint_whois.py for WHOIS lookup functionality

---
+++ controllers/outros_osint/osint_whois.py
@@ -0,0 +1,17 @@
+from ui.menus import menu_osint 
+from ui.mensagens import erro, outro
+from core.clear import clear
+from modulos.osint.whois import BuscarWhois 
+
+def painel_whois():
+  while True:
+    try:
+      print(menu_osint)
+      escolha = input("[0] sair or domain/ip > ")
+      if escolha == "0":
+        clear()
+        break
+      user = BuscarWhois(escolha)
+      user.Buscar()
+      outro()
+      continue 
