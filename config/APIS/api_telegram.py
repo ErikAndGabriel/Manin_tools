83aa69a5886d52917af0efd3d521e24b42b4bfdc
erikandgabriel <eeu31471@gmail.com>
2026-08-05 01:02:00 -0300

    Update api_telegram.py

--- config/APIS/api_telegram.py
+++ config/APIS/api_telegram.py
@@ -1,1 +1,15 @@
-.
+APIS_TELEGRAM = {
+
+  "info_bot": {
+    "GetMe": "https://api.telegram.org/bot{}/getMe",
+    "getWebhookInfo": "https://api.telegram.org/bot{}/getWebhookInfo",
+    "getMyName": "https://api.telegram.org/bot{}/getMyName",
+    "getMyDescription": "https://api.telegram.org/bot{}/getMyDescription",
+    "getMyShortDescription": "https://api.telegram.org/bot{}/getMyShortDescription",
+    "getMyCommands": "https://api.telegram.org/bot{}/getMyCommands",
+    "getUpdates": "https://api.telegram.org/bot{}/getUpdates",
+  },
+  "info_chat": {}
+}
+
+
