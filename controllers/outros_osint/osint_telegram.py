5c5b50476138c1ad7b561e8b5e5c5e8302d9cbf8
erikandgabriel <eeu31471@gmail.com>
2026-08-08 18:03:59 -0300

    Update osint_telegram.py

--- controllers/outros_osint/osint_telegram.py
+++ controllers/outros_osint/osint_telegram.py
@@ -1,11 +1,36 @@
 from ui.menus import menu_telegram
-from ui.prompt import prompt_osint 
+from ui.prompt import prompt_telegram 
 from ui.mensagens import erro, outro
-from core.clear import clear
+from core.clear import clear 
 from modulos.osint.telegram_info_bot import BotTelegramInfo
 from modulos.osint.telegram_info_chat import ChatTelegramInfo
 
 def painel_telegram():
   while True:
     try:
-      print(menu_telegram)     
+      clear()
+      print(menu_telegram) 
+      escolha = int(input(prompt_telegram))
+      if escolha == 1:
+        chat_id = input("chat id", prompt_telegram)
+        bot_token = input("bot token", prompt_telegram)
+        user = ChatTelegramInfo(bot_token, chat_id)
+        user.Buscar()
+        outro()
+        
+      elif escolha == 2:
+        bot_token = input("bot token", prompt_telegram)
+        user = BotTelegramInfo(bot_token)
+        user.buscar()
+        outro()
+        
+      else:
+        erro("escolha invalida")
+        outro()
+    except ValueError:
+      erro("somente numeros")
+      outro()
+    except KeyboardInterrupt:
+      break
+    except Exception as e:
+      erro(f"erro: {e}")
