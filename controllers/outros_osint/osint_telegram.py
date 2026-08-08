8b815ed00632e8312d913f9498ff19bdf5f0e943
erikandgabriel <eeu31471@gmail.com>
2026-08-08 17:24:14 -0300

    Update import and print statements in osint_telegram.py

--- controllers/outros_osint/osint_telegram.py
+++ controllers/outros_osint/osint_telegram.py
@@ -1,7 +1,6 @@
-from ui.menus import menu_osint 
+from ui.menus import menu_telegram
 from ui.prompt import prompt_osint 
 from ui.mensagens import erro, outro
-from ui.banner import banner_execucao1 
 from core.clear import clear
 from modulos.osint.telegram_info_bot import BotTelegramInfo
 from modulos.osint.telegram_info_chat import ChatTelegramInfo
@@ -9,5 +8,4 @@ from modulos.osint.telegram_info_chat import ChatTelegramInfo
 def painel_telegram():
   while True:
     try:
-      print(banner_execucao1)
-      
+      print(menu_telegram)     
