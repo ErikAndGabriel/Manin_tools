d9e0ae806546887e80cb5b1a3d095b0dec36faf3
erikandgabriel <eeu31471@gmail.com>
2026-08-08 17:20:05 -0300

    Update menus.py

--- ui/menus.py
+++ ui/menus.py
@@ -13,6 +13,41 @@ HOME
 ├── {azul}[09]{reset} DOX
 └── {azul}[00]{reset} EXIT
 """
+
+menu_telegram = f"""{reset}
+HOME
+│
+├── [02] OSINT
+│   ├── [01] IP INFO
+│   ├── [02] EMAIL (Holehe)
+│   ├── [03] CPF
+│   ├── [04] WHOIS
+│   ├── [05] TELEGRAM
+│   │   ├── {azul}[01]{reset} INFORMAÇÕES DO CHAT
+│   │   ├── {azul}[02]{reset} INFORMAÇÕES DO BOT
+│   │   ├── {azul}[03]{reset} INFORMAÇÕES DO USUÁRIO
+│   │   └── {azul}[00]{reset} VOLTAR
+│   ├── [06] NOME
+│   ├── [07] USERNAME
+│   ├── [08] CNPJ
+│   ├── [09] PHONE (API)
+│   ├── [10] CEP
+│   ├── [11] DDD / DDI
+│   ├── [12] NOME PAI
+│   ├── [13] GITHUB
+│   ├── [14] INSTAGRAM
+│   ├── [15] NOME MÃE
+│   ├── [16] DNS
+│   ├── [17] DNS
+│   ├── [18] CNS
+│   ├── [19] REDDIT
+│   ├── [20] CEP (API)
+│   ├── [21] RG
+│   ├── [22] PHONE
+│   ├── [23] DISCORD
+│   └── [00] VOLTAR
+"""
+
 menu_phishing = f"""{reset}
 HOME
 │
