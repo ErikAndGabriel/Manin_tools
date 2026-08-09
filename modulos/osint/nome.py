d07a0fcad20678e0c5e7137b28e7ce7f29f8c640
erikandgabriel <eeu31471@gmail.com>
2026-08-09 01:47:27 -0300

    Refactor search method with timeout and clear
    Updated search function to use timeout for sleep interval and added clear function call.

--- modulos/osint/nome.py
+++ modulos/osint/nome.py
@@ -7,15 +7,17 @@ class NomeBusca:
     self.nome = nome
     self.quantidade = quantidade
     self.timeout = timeout("Google")
-    self.user_agente = headers("aleatorio")
     self.enumerar = 0
   def Buscar(self):
+    clear()
+    print(banner_execucao1)
     for resultado in search(self.nome,
                       advanced=True,
                       num_results=self.quantidade,
-                      sleep_interval=2):
+                      sleep_interval=self.timeout):
         print(f"============ {self.enumerar} =========")
         print(f"url.      : {resultado.url}")
         print(f"titulo.   : {resultado.title}")
         print(f"descrição : {resultado.description}")
         
+#tão tedioso, igual a sua vida 
