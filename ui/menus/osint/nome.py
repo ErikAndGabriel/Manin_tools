c6c3c0da4e02a5773c8ccfd5005921b107bc5e24
erikslava <eeu31471@gmail.com>
2026-09-05 23:53:38 -0300

    Create nome.py

---
+++ ui/menus/osint/nome.py
@@ -0,0 +1,32 @@
+menu_nome = f"""{reset}
+HOME
+│
+├── [02] OSINT
+│   ├── [01] IP INFO
+│   ├── [02] EMAIL (Holehe)
+│   ├── [03] CPF
+│   ├── [04] WHOIS
+│   ├── [05] TELEGRAM
+│   ├── [06] NOME
+│   │   ├── {azul}[01]{reset} GOOGLE DORK
+│   │   ├── {azul}[02]{reset} SISP-NI
+│   │   └── {azul}[00]{reset} SAIR
+│   ├── [07] USERNAME
+│   ├── [08] CNPJ
+│   ├── [09] PHONE (API)
+│   ├── [10] CEP
+│   ├── [11] DDD / DDI
+│   ├── [12] NOME PAI
+│   ├── [13] GITHUB
+│   ├── [14] INSTAGRAM
+│   ├── [15] NOME MÃE
+│   ├── [16] DNS
+│   ├── [17] DNS
+│   ├── [18] CNS
+│   ├── [19] REDDIT
+│   ├── [20] CEP (API)
+│   ├── [21] RG
+│   ├── [22] PHONE
+│   ├── [23] DISCORD
+│   └── [00] VOLTAR
+"""
