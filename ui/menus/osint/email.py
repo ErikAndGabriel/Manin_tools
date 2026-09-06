485279b2ba32db59f138e92e00b53f6cc1c53b7a
erikslava <eeu31471@gmail.com>
2026-09-05 23:51:34 -0300

    Create email.py for OSINT email menu
    Added a menu for email OSINT options including various queries.

---
+++ ui/menus/osint/email.py
@@ -0,0 +1,31 @@
+menu_email = f"""{reset}
+HOME
+│
+├── [02] OSINT
+│   ├── [01] IP INFO
+│   ├── [02] EMAIL (Holehe)
+│   │   ├── {azul}[01]{reset} CONSULTAR EMAIL
+│   │   └── {azul}[00]{reset} SAIR
+│   ├── [03] CPF
+│   ├── [04] WHOIS
+│   ├── [05] TELEGRAM
+│   ├── [06] NOME
+│   ├── [07] USERNAME
+│   ├── [08] CNPJ
+│   ├── [09] PHONE (API)
+│   ├── [10] CEP
+│   ├── [11] DDD / DDI
+│   ├── [12] NOME PAI
+│   ├── [13] GITHUB
+│   ├── [14] INSTAGRAM
+│   ├── [15] NOME MÃE
+│   ├── [16] DNS
+│   ├── [17] DNS
+│   ├── [18] CNS
+│   ├── [19] REDDIT
+│   ├── [20] CEP (API)
+│   ├── [21] RG
+│   ├── [22] PHONE
+│   ├── [23] DISCORD
+│   └── [00] VOLTAR
+"""
