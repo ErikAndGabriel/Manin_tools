804a437880f033f43769614aaa69938e25d8f524
erikslava <eeu31471@gmail.com>
2026-09-05 23:19:52 -0300

    Update telegram.py

--- ui/menus/osint/telegram.py
+++ ui/menus/osint/telegram.py
@@ -1,1 +1,109 @@
-.
+menu_telegram = f"""{reset}
+HOME
+│
+├── [02] OSINT
+│   ├── [01] IP INFO
+│   ├── [02] EMAIL (Holehe)
+│   ├── [03] CPF
+│   ├── [04] WHOIS
+│   ├── [05] TELEGRAM
+│   │   ├── {azul}[01]{reset} TELEGRAM BOT (TOKEN)
+│   │   ├── {azul}[02]{reset} TELEGRAM CONTA (TOKEN/SESSÃO)
+│   │   └── {azul}[00]{reset} VOLTAR
+│   ├── [06] NOME
+│   ├── [07] USERNAME
+│   ├── [08] CNPJ
+│   ├── [09] PHONE (API)
+│   ├── [10] CEP
+│   ├── [11] DDD / DDI
+│   ├── [12] NOME PAI
+│   ├── [13] GITHUB
+│   ├── [14] INSTAGRAM
+│   ├── [15] NOME MÃE
+│   ├── [16] DNS
+│   ├── [17] DNS
+│   ├── [18] CNS
+│   ├── [19] REDDIT
+│   ├── [20] CEP (API)
+│   ├── [21] RG
+│   ├── [22] PHONE
+│   ├── [23] DISCORD
+│   └── [00] VOLTAR
+"""
+
+menu_telegram = f"""{reset}
+HOME
+│
+├── [02] OSINT
+│   ├── [01] IP INFO
+│   ├── [02] EMAIL (Holehe)
+│   ├── [03] CPF
+│   ├── [04] WHOIS
+│   ├── [05] TELEGRAM
+│   │   ├── {azul}[01]{reset} TELEGRAM BOT (TOKEN)
+│   │   │   ├── [01] INFORMAÇÕES DO BOT
+│   │   │   ├── [02] INFORMAÇÕES DO CHAT
+│   │   │   ├── [03] MENSAGENS
+│   │   │   ├── [04] COMANDOS
+│   │   │   └── [00] VOLTAR
+│   │   ├── {azul}[02]{reset} TELEGRAM CONTA (TOKEN/SESSÃO)
+│   │   └── {azul}[00]{reset} VOLTAR
+│   ├── [06] NOME
+│   ├── [07] USERNAME
+│   ├── [08] CNPJ
+│   ├── [09] PHONE (API)
+│   ├── [10] CEP
+│   ├── [11] DDD / DDI
+│   ├── [12] NOME PAI
+│   ├── [13] GITHUB
+│   ├── [14] INSTAGRAM
+│   ├── [15] NOME MÃE
+│   ├── [16] DNS
+│   ├── [17] DNS
+│   ├── [18] CNS
+│   ├── [19] REDDIT
+│   ├── [20] CEP (API)
+│   ├── [21] RG
+│   ├── [22] PHONE
+│   ├── [23] DISCORD
+│   └── [00] VOLTAR
+"""
+
+menu_telegram = f"""{reset}
+HOME
+│
+├── [02] OSINT
+│   ├── [01] IP INFO
+│   ├── [02] EMAIL (Holehe)
+│   ├── [03] CPF
+│   ├── [04] WHOIS
+│   ├── [05] TELEGRAM
+│   │   ├── {azul}[01]{reset} TELEGRAM BOT (TOKEN)
+│   ├── {azul}[02]{reset} TELEGRAM CONTA (TOKEN/SESSÃO)
+│   │   ├── {azul}[01]{reset} INFORMAÇÕES DE USUÁRIO 
+│   │   ├── {azul}[02]{reset} INFORMAÇÕES DE CHAT
+│   │   ├── {azul}[03]{reset} TESTAR SESSÃO
+│   │   └── {azul}[00]{reset} VOLTAR
+│   │
+│   └── {azul}[00]{reset} VOLTAR
+│
+├── [06] NOME
+├── [07] USERNAME
+├── [08] CNPJ
+├── [09] PHONE (API)
+├── [10] CEP
+├── [11] DDD / DDI
+├── [12] NOME PAI
+├── [13] GITHUB
+├── [14] INSTAGRAM
+├── [15] NOME MÃE
+├── [16] DNS
+├── [17] DNS
+├── [18] CNS
+├── [19] REDDIT
+├── [20] CEP (API)
+├── [21] RG
+├── [22] PHONE
+├── [23] DISCORD
+└── [00] VOLTAR
+"""
