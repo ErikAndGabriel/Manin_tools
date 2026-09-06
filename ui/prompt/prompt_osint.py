ae3a4404976c186111c43ff3908b4010f056edde
erikslava <eeu31471@gmail.com>
2026-09-06 00:21:41 -0300

    Add OSINT prompts for various modules

---
+++ ui/prompt/prompt_osint.py
@@ -0,0 +1,12 @@
+from ui.color import azul, verde, reset 
+
+prompt_telegram = f"{azul}Olhar {verde}modulos/osint/telegram {azul}>{reset} "
+prompt_telegram_bot = f"{azul}Olhar {verde}modulos/osint/telegram/bot {azul}>{reset} "
+prompt_telegram_conta = f"{azul}Olhar {verde}modulos/osint/telegram/conta {azul}>{reset} "
+prompt_ip = f"{azul}Olhar {verde}modulos/osint/ip {azul}>{reset} "
+prompt_cep = f"{azul}Olhar {verde}modulos/osint/cep {azul}>{reset} "
+prompt_email = f"{azul}Olhar {verde}modulos/osint/email {azul}>{reset} "
+prompt_cpf = f"{azul}Olhar {verde}modulos/osint/cpf {azul}>{reset} "
+prompt_nome = f"{azul}Olhar {verde}modulos/osint/nome {azul}>{reset} "
+prompt_whois = f"{azul}Olhar {verde}modulos/osint/whois {azul}>{reset} "
+
