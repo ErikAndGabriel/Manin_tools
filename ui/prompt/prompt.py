12ba4c9e1bb3fbe83d4e93cc090b6f1460486b09
erikslava <eeu31471@gmail.com>
2026-09-06 00:24:00 -0300

    Create prompt.py

---
+++ ui/prompt/prompt.py
@@ -0,0 +1,9 @@
+from ui.color import azul, verde, vermelho, reset, 
+
+prompt_menu = f"{azul}Olhar {verde}~/ {azul}> "
+prompt_phishing = f"{azul}Olhar {verde}modulos/phishing {azul}>{reset} "
+prompt_network = f"{azul}Olhar {verde}modulos/network {azul}>{reset} "
+prompt_osint = f"{azul}Olhar {verde}modulos/osint {azul}>{reset} "
+prompt_web = f"{azul}Olhar {verde}modulos/web {azul}>{reset} "
+prompt_lorkup = f"{azul}Olhar {verde}modulos/lorkup {azul}>{reset} "
+prompt_malware = f"{vermelho}Olhar {azul}modulos/malware {vermelho}>{reset} "
