c98f04e70cd53308e6a2314664dd7eaa1dfd7dec
erikslava <eeu31471@gmail.com>
2026-09-05 23:25:17 -0300

    Add main menu structure to menus.py

---
+++ ui/menus/menus.py
@@ -0,0 +1,14 @@
+menu_principal = f"""{reset}
+HOME
+│
+├── {azul}[01]{reset} PHISHING.     
+├── {azul}[02]{reset} OSINT
+├── {azul}[03]{reset} NETWORK
+├── {azul}[04]{reset} INSTALL
+├── {azul}[05]{reset} MALWARES
+├── {azul}[06]{reset} C2
+├── {azul}[07]{reset} WEB
+├── {azul}[08]{reset} LORKUP
+├── {azul}[09]{reset} DOX
+└── {azul}[00]{reset} EXIT
+"""
