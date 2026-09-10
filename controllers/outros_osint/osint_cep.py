1994cfa10039728fcc06d8f1c9dbe1e05f7b6a51
erikslava <eeu31471@gmail.com>
2026-09-10 02:45:28 -0300

    Fix import path for prompt_osint in osint_cep.py

--- controllers/outros_osint/osint_cep.py
+++ controllers/outros_osint/osint_cep.py
@@ -1,5 +1,5 @@
 from ui.menus.osint.cep import menu_cep
-from ui.prompt import prompt_osint
+from ui.prompt.prompt import prompt_osint
 from ui.mensagens import erro, outro
 from ui.color import azul
 from modulos.osint.cep import Cep
