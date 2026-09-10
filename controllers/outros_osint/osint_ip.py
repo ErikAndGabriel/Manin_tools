9f3fd9d16a3b4a3b03dd3c23aeb1ea62b86f9242
erikslava <eeu31471@gmail.com>
2026-09-10 02:46:33 -0300

    Fix import path for prompt_ip in osint_ip.py

--- controllers/outros_osint/osint_ip.py
+++ controllers/outros_osint/osint_ip.py
@@ -1,5 +1,5 @@
 from ui.menus.osint.ip import menu_ip
-from ui.prompt import prompt_ip
+from ui.prompt.prompt import prompt_ip
 from ui.mensagens import erro, outro
 from modulos.osint.ip import Ip
 from core.clear import clear
