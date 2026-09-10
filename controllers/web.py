efc3f17b399f3a7b789a8ced83e828983da85a47
erikslava <eeu31471@gmail.com>
2026-09-10 02:45:09 -0300

    Fix import path for prompt_web in web.py

--- controllers/web.py
+++ controllers/web.py
@@ -1,5 +1,5 @@
 from ui.menus.menus import menu_web
-from ui.prompt import prompt_web
+from ui.prompt.prompt import prompt_web
 from ui.mensagens import erro
 from core.carregar import carregar_json 
 from core.clear import clear
