750dfa722eb3c2c3679e909b978feaa0f1052c1e
erikslava <eeu31471@gmail.com>
2026-09-17 23:39:28 -0300

    Update api_bin.py

--- config/APIS/api_bin.py
+++ config/APIS/api_bin.py
@@ -7,16 +7,8 @@ APIS_BIN = {
         "nome": "BINCheck",
         "url": "https://bincheck.io/api/v1/{}"
     },
-    "bins": {
-        "nome": "BINS",
-        "url": "https://api.bins.dev/{}"
-    },
     "freebinchecker": {
         "nome": "Free BIN Checker",
         "url": "https://api.freebinchecker.com/bin/{}"
-    },
-    "payout": {
-        "nome": "Payout BINs",
-        "url": "https://bins.payout.com/api/v1/bins/{}"
     }
 }
