17a6cbe56a692210100657cf17df1aaa96de3eb3
ErikAndGabriel <erikSlava@proton.me>
2026-09-21 00:03:24 -0300

    erro ao chamar o arq json, s variavel de nome (datas), ao tentar pergar seus valores {data[nome]}, deveria ser {datas[nome]}

--- ui/banner.py
+++ ui/banner.py
@@ -1,7 +1,7 @@
 from ui.color import vermelho 
 from core.carregar import carregar_json 
 
-datas = carregar_json("config/app.json")
+data = carregar_json("config/app.json")
 banner_principal = f"""{vermelho}
 ⠀⠀⠀         ⠀⣀⣠⣤⣶⣶⣶⣶⣶⣶⣶⣦⣀⠀⠀⠀⠀⢀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⢠⢤⣠⣶⣿⣿⡿⠿⠛⠛⠛⠛⠉⠛⠛⠛⠛⠿⣷⡦⠞⣩⣶⣸⡆⠀⠀⠀⠀⠀⠀⠀
@@ -23,7 +23,7 @@ banner_principal = f"""{vermelho}
 ⠀⠀⠀⠀⠀⠀⠀⠈⠙⠿⣿⣿⣿⣼⣿⣿⣟⠀⠀⡠⠊⠀⣀⣀⣠⣴⣶⠿⠟⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠛⠿⣿⣿⣿⣿⣶⣶⣷⣶⣶⡿⠿⠛⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠛⠛⠛⠛⠋
-    ⠀  {data["nome"]}  {data["version"]}⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+       ⠀  {data["nome"]}  {data["version"]}⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 """
 
 banner_execucao1 = f"""{vermelho}
