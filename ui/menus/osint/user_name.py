fd651de88fe56533b8304be1cb5fb31abaef4369
ErikAndGabriel <erikSlava@proton.me>
2026-09-21 01:09:44 -0300

    menu para o user name

---
+++ ui/menus/osint/user_name.py
@@ -0,0 +1,34 @@
+from ui.color import azul, reset
+
+menu_cep = f"""{reset}
+HOME
+│
+├─
+├── [02] OSINT
+│   ├── [01] IP INFO
+│   ├── [02] EMAIL (Holehe)
+│   ├── [03] CPF
+│   ├── [04] WHOIS
+│   ├── [05] TELEGRAM
+│   ├── [06] NOME
+│   ├── [07] USERNAME
+│   │   ├── {azul}[01]{reset} BUSCAR USERNAME
+│   │   └── {azul}[00]{reset} VOLTAR
+│   ├── [08] CNPJ
+│   ├── [09] PHONE (API)
+│   ├── [10] CEP
+│   ├── [11] DDD / DDI
+│   ├── [12] NOME PAI
+│   ├── [13] GITHUB
+│   ├── [14] INSTAGRAM
+│   ├── [15] NOME MÃE
+│   ├── [16] BIN
+│   ├── [17] DNS
+│   ├── [18] CNS
+│   ├── [19] REDDIT
+│   ├── [20] CEP (API)
+│   ├── [21] RG
+│   ├── [22] PHONE
+│   ├── [23] DISCORD
+│   └── [00] VOLTAR
+"""
