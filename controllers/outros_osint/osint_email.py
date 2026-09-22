e9d01ade84b79cd2c11c9a8d86a58023fdd9615a
ErikAndGabriel <erikSlava@proton.me>
2026-09-22 18:21:51 -0300

    bug arrumado

--- controllers/outros_osint/osint_email.py
+++ controllers/outros_osint/osint_email.py
@@ -8,13 +8,23 @@ def painel_email():
   while True:
     try:
       print(menu_email)
-      email = input(prompt_email)
-      if email == "0":
+      escolha = int(input(prompt_email))
+      if escolha == 0:
         clear()
         break
-      user = Email(email)
-      outro()
-      continue
+      elif escolha == 1:
+        email = input(f"email {prompt_email}")
+        user = Email(email)
+        outro()
+        continue
+      else:
+        erro("escolha invalida")
+        outro()
+        continue
     except Exception as e:
       erro(f"erro: {e}")
+      continue
+    except ValueError:
+      erro("somente numeros!")
+      outro()
       continue
